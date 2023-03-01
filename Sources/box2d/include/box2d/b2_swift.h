9991780aa3b3c59b1e953c249b99845e3db9c28a
Владислав Прусаков <v.prusakov@corp.mail.ru>
2023-03-01 17:37:14 +0300

    add more c functions for swift compatibility

--- Sources/box2d/include/box2d/b2_swift.h
+++ Sources/box2d/include/box2d/b2_swift.h
@@ -23,10 +23,11 @@
 #include "b2_body.h"
 #include "b2_shape.h"
 #include "b2_joint.h"
+#include "b2_world.h"
 
 // b2Body
 
-const b2Vec2 b2Body_GetPosition(const b2Body *body) {
+const b2Vec2& b2Body_GetPosition(const b2Body *body) {
     return body->GetPosition();
 }
 
@@ -38,6 +39,14 @@ b2Fixture* b2Body_GetFixtureList(b2Body *body) {
     return body->GetFixtureList();
 }
 
+b2Fixture* b2Body_CreateFixture(b2Body *body, const b2FixtureDef* def) {
+    return body->CreateFixture(def);
+}
+
+void b2Body_DestroyFixture(b2Body *body, b2Fixture* fixture) {
+    body->DestroyFixture(fixture);
+}
+
 void b2Body_SetTransform(b2Body *body, const b2Vec2& position, float angle) {
     body->SetTransform(position, angle);
 }
@@ -46,11 +55,11 @@ void b2Body_SetLinearVelocity(b2Body *body, const b2Vec2& v) {
     body->SetLinearVelocity(v);
 }
 
-const b2Vec2 b2Body_GetWorldCenter(const b2Body *body) {
+const b2Vec2& b2Body_GetWorldCenter(const b2Body *body) {
     return body->GetWorldCenter();
 }
 
-const b2Vec2 b2Body_GetLocalCenter(const b2Body *body) {
+const b2Vec2& b2Body_GetLocalCenter(const b2Body *body) {
     return body->GetLocalCenter();
 }
 
@@ -119,13 +128,29 @@ b2Vec2 b2Body_GetLinearVelocityFromLocalPoint(const b2Body *body, const b2Vec2& 
 
 /// Get the linear velocity of the center of mass.
 /// @return the linear velocity of the center of mass.
-b2Vec2 b2Body_GetLinearVelocity(const b2Body *body) {
+const b2Vec2& b2Body_GetLinearVelocity(const b2Body *body) {
     return body->GetLinearVelocity();
+}
+
+b2BodyUserData& b2Body_GetUserData(b2Body *body) {
+    return body->GetUserData();
+}
+
+b2BodyDef* b2BodyDef_create() {
+    return new b2BodyDef();
 }
 
 // end b2Body
 
 // b2Shape
+
+b2PolygonShape* b2PolygonShape_create() {
+    return new b2PolygonShape();
+}
+
+b2CircleShape* b2CircleShape_create() {
+    return new b2CircleShape();
+}
 
 const b2Shape* b2Shape_unsafeCast(void *shape) {
     return (const b2Shape *)shape;
@@ -150,6 +175,10 @@ const b2JointDef* b2JointDef_unsafeCast(void *joint) {
 class ContactListener2D: public b2ContactListener {
 public:
     ContactListener2D(const void *userData): m_UserData(userData) {}
+    
+    ~ContactListener2D() {
+        
+    }
     
     virtual void BeginContact(b2Contact* contact) override {
         m_BeginContact(m_UserData, contact);
@@ -192,8 +221,75 @@ private:
     const void* m_UserData;
 };
 
+ContactListener2D* ContactListener2D_create(const void *userData) {
+    return new ContactListener2D(userData);
+}
+
 b2ContactListener* b2ContactListener_unsafeCast(void *ptr) {
     return (b2ContactListener*)ptr;
 }
 
 // b2ContactListner end
+
+// b2World
+
+b2World* b2World_create(const b2Vec2& gravity) {
+    return new b2World(gravity);
+}
+
+b2Vec2 b2World_GetGravity(const b2World* world) {
+    return world->GetGravity();
+}
+
+void b2World_SetGravity(b2World* world, const b2Vec2& gravity) {
+    world->SetGravity(gravity);
+}
+
+/// Register a contact event listener. The listener is owned by you and must
+/// remain in scope.
+void b2World_SetContactListener(b2World* world, b2ContactListener* listener) {
+    world->SetContactListener(listener);
+}
+
+/// Take a time step. This performs collision detection, integration,
+/// and constraint solution.
+/// @param timeStep the amount of time to simulate, this should not vary.
+/// @param velocityIterations for the velocity constraint solver.
+/// @param positionIterations for the position constraint solver.
+void b2World_Step(
+                  b2World* world,
+                  float timeStep,
+                  int32 velocityIterations,
+                  int32 positionIterations) {
+    world->Step(timeStep, velocityIterations, positionIterations);
+}
+
+/// Create a rigid body given a definition. No reference to the definition
+/// is retained.
+/// @warning This function is locked during callbacks.
+b2Body* b2World_CreateBody(b2World* world, const b2BodyDef* def) {
+    return world->CreateBody(def);
+}
+
+/// Destroy a rigid body given a definition. No reference to the definition
+/// is retained. This function is locked during callbacks.
+/// @warning This automatically deletes all associated shapes and joints.
+/// @warning This function is locked during callbacks.
+void b2World_DestroyBody(b2World* world, b2Body* body) {
+    world->DestroyBody(body);
+}
+
+/// Create a joint to constrain bodies together. No reference to the definition
+/// is retained. This may cause the connected bodies to cease colliding.
+/// @warning This function is locked during callbacks.
+b2Joint* b2World_CreateJoint(b2World* world, const b2JointDef* def) {
+    return world->CreateJoint(def);
+}
+
+/// Destroy a joint. This may cause the connected bodies to begin colliding.
+/// @warning This function is locked during callbacks.
+void b2World_DestroyJoint(b2World* world, b2Joint* joint) {
+    world->DestroyJoint(joint);
+}
+
+// b2World end
